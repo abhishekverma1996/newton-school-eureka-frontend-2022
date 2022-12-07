333141b0049133a8d4c934a0a942381b7cf863e4
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2022-12-07 10:53:28 +0530

    Create JS callbacks.js

---
+++ JS Hoisting, Callback function, JS Control Flow - Post Class/JS callbacks.js
@@ -0,0 +1,9 @@
+function callThisFnBack(a,add) {
+        // return the output using return keyword
+    // do not console.log it
+     return add(a);
+}
+function add(num){
+    return num+num;
+
+}
