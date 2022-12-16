09a3646e809c771b5e1cff85ee47a8010e3f7075
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2022-12-16 17:21:33 +0530

    Create JS Math - 4.js

---
+++ JS Math, JS String, JS Date - Post Class/JS Math - 4.js
@@ -0,0 +1,7 @@
+function sumMaxMin(a,b,c,d,e){
+    // write code here
+    // return the output , do not use console.log her
+  let max =Math.max(a,b,c,d,e);
+  let min =Math.min(a,b,c,d,e);
+  return(min + max);
+}
