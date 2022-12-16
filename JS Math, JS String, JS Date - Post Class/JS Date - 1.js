b7af32a6292e94a6b76558bc91f58f8b96b93e1f
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2022-12-16 17:22:31 +0530

    Create JS Date - 1.js

---
+++ JS Math, JS String, JS Date - Post Class/JS Date - 1.js
@@ -0,0 +1,9 @@
+function msSinceEpoch() {
+    // write code here
+    // return the output , do not use console.log here
+    let epoch= new Date(0);
+    let now= new Date();
+    let milliseconds=now - epoch;
+return milliseconds; 
+    
+}
