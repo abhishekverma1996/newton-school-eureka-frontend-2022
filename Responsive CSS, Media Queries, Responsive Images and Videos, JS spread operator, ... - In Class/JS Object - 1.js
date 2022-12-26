5cd8f5951879061c8c990606837274c123d259a5
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2022-12-26 22:21:39 +0530

    Create JS Object - 1.js

---
+++ Responsive CSS, Media Queries, Responsive Images and Videos, JS spread operator, ... - In Class/JS Object - 1.js
@@ -0,0 +1,10 @@
+function createUserObj(email,password){
+    // write code here
+   // return the output, do not console.log it
+var obj = {
+    "email":  email,
+    "password": password
+  };
+  // Return it
+  return obj;
+}
